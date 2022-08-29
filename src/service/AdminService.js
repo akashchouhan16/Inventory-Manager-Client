e1d68a511e3303fe0de90ae4db3f1ca593bfd54f
akashchouhan16 <akash.c1500@gmail.com>
2022-08-29 12:01:25 +0530

    :hammer: updated service integration

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
-const GET_SELLERS_API = 'http://10.30.1.144:8007/admin/getAllUsers'
-const DISBALE_SELLER_API = 'http://10.30.1.144:8007/admin/updateStatus'
-
+// const GET_SELLERS_API = 'http://10.30.1.144:8007/admin/getAllUsers'
+const GET_SELLERS_API = 'http://10.30.1.35:8005/admin/sellers'
+const DISBALE_SELLER_API = 'http://10.30.1.35:8005/admin/status/'
+const SELLER_SEARCH_API = 'http://10.30.1.35:8005/admin/sellers/'
 
 export const getAllSellers = ({success, error})=>{
     axios.get(GET_SELLERS_API)
@@ -15,11 +16,21 @@ export const getAllSellers = ({success, error})=>{
 }
 
 export const disbaleSeller = ({success, error, userId}) =>{
-   axios.put(DISBALE_SELLER_API, userId)
+   axios.put(DISBALE_SELLER_API + userId, userId)
          .then((response)=>{
                success && success(response);
          })
          .catch((err)=>{
                error && error(err);
          })
+}
+
+export const getSellerFromSearch = ({success, error, searchKey}) =>{
+      axios.get(SELLER_SEARCH_API + searchKey)
+            .then((response)=>{
+                  success && success(response);
+            })
+            .catch(err =>{
+                  error && error(err);
+            })
 }
